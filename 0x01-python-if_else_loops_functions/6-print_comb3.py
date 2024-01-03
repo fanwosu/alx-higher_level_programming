1637ee6b56e8d80ea10e97e5b7ac7407be707b2f
Nwosu Friday <fanwosu@gmail.com>
2024-01-03 02:09:19 -0800

    print lowest number

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(10):
-        if j < 9:
-            print("{}{}".format(i,j), end=", ")
-        else:
-            print("{}{}".format(i,j))
+    for j in range(1,10):
+        if j > i:
+            if j < 9:
+                print("{}{}".format(i,j), end=", ")
+            else:
+                print("{}{}".format(i,j))
