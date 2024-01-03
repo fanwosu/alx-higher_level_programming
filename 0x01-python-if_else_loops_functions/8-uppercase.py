2ee838a22b2f96f21769a476a3f3746cad5bc8c9
Nwosu Friday <fanwosu@gmail.com>
2024-01-03 03:16:34 -0800

    upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-def isupper(c):
-    if ord(c) >= ord('A') and ord(c) <= ord('Z'):
-        return True
-    return False
+def uppercase(str):
+    for char in str:
+        if ord(char) >= ord('a') and ord(char) <= ord('z'):
+            char = chr(ord(char) - 32)
+        print("{:s}".format(char), end="")
+
+
