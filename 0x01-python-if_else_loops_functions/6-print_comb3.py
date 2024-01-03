d0ff2cf532269498182e832ba3d4e308e61c7800
Nwosu Friday <fanwosu@gmail.com>
2024-01-03 02:14:19 -0800

    print numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(10):
     for j in range(1,10):
         if j > i:
-            if j < 9:
+            if i < 8:
                 print("{}{}".format(i,j), end=", ")
             else:
                 print("{}{}".format(i,j))
