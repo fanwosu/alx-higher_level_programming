3c9f9a6bba12693f087a73d4932be222c55b62cd
Nwosu Friday <fanwosu@gmail.com>
2024-01-04 05:41:11 -0800

    print argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from sys import argv
 def print_arg(argv):
     n = len(argv) - 1
     if n == 0:
@@ -14,5 +13,7 @@ def print_arg(argv):
             while i <= n:
                 print("{:d}: {:s}".format(i, argv[i]))
                 i += 1
+
 if __name__ == "__main__":
+    from sys import argv
     print_arg(sys.argv)
