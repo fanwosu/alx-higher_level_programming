4ce446e4bec12b80556c31933fde977e1cac247e
Nwosu Friday <fanwosu@gmail.com>
2024-01-04 10:22:25 -0800

    add numbers

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def infinite_add(argv):
+    n = len(argv) - 1
+    if n == 0:
+        result = 0
+        print("{}".format(result))
+    else:
+        result = 0
+        for n in range(1, n + 1):
+            result += int(argv[n])
+        print("{}".format(result))
+
+if __name__ == "__main__":
+    import sys
+    infinite_add(sys.argv)
