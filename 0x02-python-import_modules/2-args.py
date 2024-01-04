6e7e2baa29d6419a8e310310d4c62ceaabbca0f1
Nwosu Friday <fanwosu@gmail.com>
2024-01-04 09:38:39 -0800

    arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ def print_arg(argv):
             print("{} argument:".format(n))
         else:
             print("{} arguments:".format(n))
-        for n in range(n + 1):
+        for n in range(1, n + 1):
             print("{}: {}".format(n, argv[n]))
 
 if __name__ == "__main__":
