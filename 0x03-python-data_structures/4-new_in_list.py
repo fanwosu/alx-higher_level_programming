78a3b10bcec5ac0a8a155e0c0b7f72b79b8dc902
Nwosu Friday <fanwosu@gmail.com>
2024-01-06 00:31:45 -0800

    new in list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     my_list_copy = []
-    if idx <= 0:
+    if idx < 0:
         for i in range(len(my_list)):
             my_list_copy = my_list[i]
         return (my_list_copy)
-    if idx > len(my_list):
+    if idx => len(my_list):
         for i in range(len(my_list)):
             my_list_copy = my_list[i]
         return (my_list_copy)
