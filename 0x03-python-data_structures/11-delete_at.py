d2df46f853272cd630eba72100ca1bb3d04df438
Nwosu Friday <fanwosu@gmail.com>
2024-01-06 04:00:31 -0800

    delete at index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,9 +2,7 @@
 
 def delete_at(my_list=[], idx=0):
     n = len(my_list)
-    if idx < 0 or idx >= n:
+    if idx < 0 or idx > n:
         return (my_list)
-    for i in range(n):
-        if idx == i:
-            my_list.remove(idx)
+    my_list.remove(idx)
     return (my_list)
