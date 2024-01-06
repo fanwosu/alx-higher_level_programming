a56e6dc9e4ce11417f99b90de09cb1eb20a7916d
Nwosu Friday <fanwosu@gmail.com>
2024-01-06 03:01:13 -0800

    max integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
     n = len(my_list)
     max = my_list[0]
     if n == 0:
-        return ('None')
+        return (None)
     for i in range(n):
         if my_list[i] > max:
             max = my_list[i]
