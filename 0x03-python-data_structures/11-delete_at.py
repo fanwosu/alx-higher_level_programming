b6d0a2d3da8e9c8881d2e11e82dd0c1c531e3c60
Nwosu Friday <fanwosu@gmail.com>
2024-01-06 03:48:27 -0800

    delete element

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,8 +4,7 @@ def delete_at(my_list=[], idx=0):
     n = len(my_list)
     if idx < 0 or idx > n:
         return (my_list)
-    new_list = []
     for i in range(n):
-        if idx != i:
-            new_list.append(my_list[i])
-    return (new_list)
+        if idx == i:
+            my_list.remove(idx)
+    return (my_list)
