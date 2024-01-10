ef3661885aa807183277346cea23978d8b28d702
Nwosu Friday <fanwosu@gmail.com>
2024-01-10 14:04:51 -0800

    sorted keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_dic = sorted(a_dictionary.keys())
-    return (sorted_dic)
+    sorted_dic = sorted(a_dictionary)
+    for key in sorted_dic:
+        value = a_dictionary[key]
+        print(f"{key}: {value}")
