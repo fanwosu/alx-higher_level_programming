6fe31ffe3005d3cee9bf8dbccc89aac0b0f3c470
Nwosu Friday <fanwosu@gmail.com>
2024-01-10 07:50:14 -0800

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    result = my_list[:]
+    for i in range(len(result)):
+        if result[i] == search:
+            result[i] = replace
+    return (result)
