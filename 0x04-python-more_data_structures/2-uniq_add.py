65481fbb9b38784a82347129fed25383f5a6a0b4
Nwosu Friday <fanwosu@gmail.com>
2024-01-10 08:17:49 -0800

    set

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_my_list = set()
-    for i in my_list:
-        unique_my_list.add(i)
-        result = sum(unique_my_list)
+    unique_my_list = set(my_list)
+    result = sum(unique_my_list)
     return (result)
