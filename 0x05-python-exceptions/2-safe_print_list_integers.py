d7c974f3e6d75cf381157c9ba83477468e73fb69
Nwosu Friday <fanwosu@gmail.com>
2024-01-22 10:08:29 -0800

    print list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        value = my_list[i]
+        try:
+            print("{:d}".format(value), end="")
+            count += 1
+        except (TypeError, ValueError):
+            pass
+    print()
+    return (count)            
