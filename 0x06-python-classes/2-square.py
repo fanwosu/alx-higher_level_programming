2d37816f266b561395708d60a349f5713a10752f
Nwosu Friday <fanwosu@gmail.com>
2024-01-23 05:23:05 -0800

    square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,13 +4,10 @@
 
 class Square:
     """Defines a sqaure with private instance attribute"""
+
     def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be an integer")
         self.__size = size
-        try:
-            print("{:d}".format(size))
-            try:
-                size < 0
-            except ValueError:
-                print("size must be >= 0")
-        except TypeError:
-            print("size must be an integer")
