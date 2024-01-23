a5066dd05754b06e4b0edd3bfbcea57069f6bd15
Nwosu Friday <fanwosu@gmail.com>
2024-01-23 07:17:28 -0800

    printing squares

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""defines a square attribute: size and instantiation"""
+
+
+class Square:
+    """Defines a sqaure with private instance attribute"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """calculates and returns current square area"""
+
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if value == 0:
+            print()
+        else:
+            print("#{}".format(self.__size ** 2))
