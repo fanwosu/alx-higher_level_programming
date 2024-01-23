40afd5e8e944ed4e02930b9777419b729d5c3574
Nwosu Friday <fanwosu@gmail.com>
2024-01-23 05:24:48 -0800

    square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,6 +8,6 @@ class Square:
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise TypeError("size must be an integer")
         self.__size = size
