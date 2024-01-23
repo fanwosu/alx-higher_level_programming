0cad5bcb27d44067918f559a1204a7949121843e
Nwosu Friday <fanwosu@gmail.com>
2024-01-23 05:42:04 -0800

    area of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines a square attribute: size and instantiation"""
+
+
+class Square:
+    """Defines a sqaure with private instance attribute"""
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (size ** 2)
