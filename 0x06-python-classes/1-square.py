f6708fdc55ad28b2c4be891df730c836cff63420
Nwosu Friday <fanwosu@gmail.com>
2024-01-23 05:03:11 -0800

    square attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines a square attribute: size and instantiation"""
+
+
+class Square:
+    """Defines a sqaure with private instance attribute"""
+    def __init__(self, size):
+        self.__size = size
