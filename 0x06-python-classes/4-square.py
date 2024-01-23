f745834e65da1e4dda4843f43e6f95e9ca8089c6
Nwosu Friday <fanwosu@gmail.com>
2024-01-23 06:26:32 -0800

    square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,19 +8,19 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
 
-        @property
-        def size(self):
-            return (self.__size)
+    @property
+    def size(self):
+        return (self.__size)
 
-        @size.setter
-        def size(self, value):
-            if type(value) is not int:
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise TypeError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = value
 
     def area(self):
-        """ calculates and returns current square area"""
+        """calculates and returns current square area"""
 
         return (self.__size ** 2)
